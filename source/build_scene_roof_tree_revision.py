307b9f1d1816242e14105d2ea7fdfcdf81e62060
XDHRY <130680817+XDHRY@users.noreply.github.com>
2026-09-23 03:42:31 +0800

    fix: place moon-gate moss stone inside sightline

--- source/build_scene_roof_tree_revision.py
+++ source/build_scene_roof_tree_revision.py
@@ -303,8 +303,8 @@ for i in range(N):
  for xx in [x-.18,x+.18]:beam('moon_gate_stone_ring',(xx,y0+r*math.cos(t),cz+r*math.sin(t)),(xx,y0+r*math.cos(u),cz+r*math.sin(u)),.065,'stone',6)
 box('garden_enclosure',(-2,9,.95),(.3,7.4,1.9),'plaster');box('garden_end',(-1,12.6,.95),(2,.3,1.9),'plaster')
 for y in np.arange(.8,12,.55):box('side_garden_path',(-1,y,.03),(1.5,.51,.06),'paving')
-sphere('moon_gate_moss_stone',(-1.0,4.70,.16),(.28,.22,.16),'stone',1)
-sphere('moon_gate_moss_cap',(-1.0,4.70,.29),(.20,.14,.05),'leaf2',1)
+sphere('moon_gate_moss_stone',(-1.0,3.52,.16),(.28,.22,.16),'stone',1)
+sphere('moon_gate_moss_cap',(-1.0,3.52,.29),(.20,.14,.05),'leaf2',1)
 for xx,yy,s in [(-4.2,6,1.1),(-3.8,10,.8),(21,10,1.2),(21,13,.85)]:tree(xx,yy,0,s)
 # Dock and piles connected to quay; boat has a hollow hull and canopy.
 layer='waterfront'
