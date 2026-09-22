db8ad504c1bda6d83827e229750b2ff812db53d8
XDHRY <130680817+XDHRY@users.noreply.github.com>
2026-09-23 04:33:04 +0800

    tune: deepen cool wet-limestone response

--- source/refine_ming.py
+++ source/refine_ming.py
@@ -31,11 +31,11 @@ source={k:Image.open(P/'textures'/v).convert('RGB') for k,v in {
 # The cool factors keep the stone/tile family blue-black without washing to neutral gray.
 base_factors={
  'tile':[185,198,208,255],
- 'paving':[208,222,230,255],
+ 'paving':[198,214,224,255],
 }
 for k,im in source.items():
  b.M[k]=PBRMaterial(name=k,baseColorTexture=im,baseColorFactor=base_factors.get(k,[255,255,255,255]),
-  roughnessFactor={'wood':.43,'woodlight':.45,'fabric':.8,'lotus':.42,'lacquer':.31,'metal':.38,'paper':.76,'tile':.34,'paving':.44}.get(k,.73),
+  roughnessFactor={'wood':.43,'woodlight':.45,'fabric':.8,'lotus':.42,'lacquer':.31,'metal':.38,'paper':.76,'tile':.34,'paving':.42}.get(k,.73),
   metallicFactor=.72 if k=='metal' else 0)
 b.COL['lotus']='875324';b.COL['brass']='b39a59';b.COL['celadon']='92bab0'
 b.M['brass']=PBRMaterial(name='brass',baseColorFactor=[179,154,89,255],roughnessFactor=.38,metallicFactor=.74)
